09bbec125ba97be6445585693e4d06ce53045be3
Pool <pool_1541@hotmail.com>
2023-10-06 19:55:48 -0500

    feat(test) : update viewport height and width in cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  viewportHeight: 846,
+  viewportWidth: 1440,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
