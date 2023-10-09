d2e5dcbf3707e365ff8d7a9d8aa494ed141e24df
Pool <pool_1541@hotmail.com>
2023-10-09 16:26:26 -0500

    feat(test): add test for the `edit a note`  use case

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -9,13 +9,29 @@ describe('Note Taking Application - Create and Modify Notes', () => {
 
   it('Should add a new note', () => {
     const noteTitle = 'New note from cypress at ' + new Date().getTime();
-
     cy.get('textarea[placeholder="Take a note..."]').click();
     cy.get('input[placeholder="Title"]').type(noteTitle);
     cy.get('textarea').type('Description from cypress');
     cy.get('button[type="submit"]').click();
 
     cy.contains(noteTitle).should('be.visible');
-    cy.contains('Description from cypress').should('be.visible');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500);
+  });
+
+  it('Should modify a note', () => {
+    const noteTitle = 'Updated at ' + new Date().getTime();
+    cy.get('button[title="Edit"]').first().click();
+
+    cy.get('.sc-hBxehG').as('titleInput');
+    cy.get('.sc-fnGiBr').as('descriptionInput');
+
+    cy.get('@titleInput').clear();
+    cy.get('@titleInput').type(noteTitle);
+    cy.get('@descriptionInput').clear();
+    cy.get('@descriptionInput').type('Description updated');
+    cy.get('.sc-jrcTuL > :nth-child(1) > button[title="Update"]').click();
+
+    cy.contains(noteTitle).should('be.visible');
   });
 });
