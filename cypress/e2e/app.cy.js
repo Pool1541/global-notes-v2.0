d2bd4ed19b22bb487e7eea04cc4856736f617fb5
Pool <pool_1541@hotmail.com>
2023-10-30 14:46:38 -0500

    test(app e2e): improve end-to-end test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,39 +1,42 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 /// <reference types="cypress" />
 
-describe('Note Taking Application - Create and Modify Notes', () => {
+describe('Notes Application - Create and Modify Notes', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.get('.esXMAm div').first().as('firstNote');
   });
 
   it('Should display the placeholder', () => {
     cy.get('textarea').should('have.attr', 'placeholder', 'Take a note...');
   });
 
+  const noteTitle = 'New note from cypress at ' + new Date().getTime();
+
   it('Should add a new note', () => {
-    const noteTitle = 'New note from cypress at ' + new Date().getTime();
     cy.get('textarea[placeholder="Take a note..."]').click();
     cy.get('input[placeholder="Title"]').type(noteTitle);
     cy.get('textarea').type('Description from cypress');
     cy.get('button[type="submit"]').click();
 
-    cy.contains(noteTitle).should('be.visible');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(500);
+    cy.wait(3000);
+
+    cy.get('@firstNote').should('include.text', noteTitle);
   });
 
   it('Should modify a note', () => {
-    const noteTitle = 'Updated at ' + new Date().getTime();
-    cy.get('button[title="Edit"]').first().click();
+    const updatedTitle = 'Updated at ' + new Date().getTime();
+    cy.get('@firstNote').find('button[title="Edit"]').click();
 
     cy.get('.sc-hBxehG').as('titleInput');
     cy.get('.sc-fnGiBr').as('descriptionInput');
 
     cy.get('@titleInput').clear();
-    cy.get('@titleInput').type(noteTitle);
+    cy.get('@titleInput').type(updatedTitle);
     cy.get('@descriptionInput').clear();
     cy.get('@descriptionInput').type('Description updated');
-    cy.get('.sc-jrcTuL > :nth-child(1) > button[title="Update"]').click();
+    cy.get('@firstNote').find('button[title="Update"]').click();
 
-    cy.contains(noteTitle).should('be.visible');
+    cy.contains(updatedTitle).should('be.visible');
   });
 });
